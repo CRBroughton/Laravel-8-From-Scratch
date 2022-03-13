5eab050debe37beab5443757551f118ad6b55def
CRBroughton <crbroughton@posteo.uk>
2022-03-13 12:01:00 +0000

    caching posts forever, and clean up files

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Support\Facades\File;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Post {
     public $title;
@@ -23,16 +22,18 @@ class Post {
     }
 
     public static function all() {
-        return collect(File::files(resource_path("posts")))
-        ->map(fn($file) => YamlFrontMatter::parseFile($file))
-        ->map(fn($document) => new Post(
-                $document->title, 
-                $document->excerpt,
-                $document->date,
-                $document->body(),
-                $document->slug
-            ))
-            ->sortByDesc('date');
+        return cache()->rememberForever('posts.all', function () {
+            return collect(File::files(resource_path("posts")))
+            ->map(fn($file) => YamlFrontMatter::parseFile($file))
+            ->map(fn($document) => new Post(
+                    $document->title, 
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                ))
+                ->sortByDesc('date');
+        });
     }
 
     public static function find($slug) {
