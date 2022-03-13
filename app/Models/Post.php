7d579796bceed51de4a48230838e74e8b9f69710
CRBroughton <crbroughton@posteo.uk>
2022-03-13 11:57:14 +0000

    updated find function to return matching slug

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -22,16 +22,6 @@ class Post {
 
     }
 
-    public static function find($slug) {
-
-        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember("posts.${slug}", 5, fn() => file_get_contents($path));
-
-    }
-
     public static function all() {
         return collect(File::files(resource_path("posts")))
         ->map(fn($file) => YamlFrontMatter::parseFile($file))
@@ -42,5 +32,9 @@ class Post {
                 $document->body(),
                 $document->slug
             ));
+    }
+
+    public static function find($slug) {
+        return static::All()->firstWhere('slug', $slug);
     }
 }
