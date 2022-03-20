4d92f15e1300979560adbf1b14fa7f37aad8a712
CRBroughton <crbroughton@posteo.uk>
2022-03-20 14:20:54 +0000

    changed blades to use layout file

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="app.css">
-</head>
-<body>
+@extends ('layout')
+
+@section ('content')
     <article>
         <h1>{{ $post->title; }}</h1>
         <div>
@@ -15,4 +8,4 @@
         </div>
     </article>
     <a href="/">Go Back</a>
-</html>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="app.css">
-</head>
-<body>
+@extends('layout')
+
+@section('content')
     @foreach ($posts as $post)
         <article>
             <h1><a href="/posts/{{ $post->slug; }}">
@@ -18,5 +11,4 @@
             </div>
         </article>
     @endforeach
-</body>
-</html>
+@endsection
