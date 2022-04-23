cb59d9e6500c1f9163584408139f7dd8e67b1be5
CRBroughton <crbroughton@posteo.uk>
2022-04-23 10:55:06 +0100

    added if check for count of posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,17 @@
 <x-layout>
     @include ('_posts-header')
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card :post="$posts[0]" />
+        @if ($posts->count())
+            <x-post-featured-card :post="$posts[0]" />
+            <div class="lg:grid lg:grid-cols-2">
+                @foreach ($posts->skip(1) as $post)
+                    <x-post-card :post="$posts[1]"/>
+                @endforeach
+            </div>
+        @else
+            <p class="text-center">No posts yet.</p>
+        @endif
 
-        <div class="lg:grid lg:grid-cols-2">
-            @foreach ($posts->skip(1) as $post)
-                <x-post-card :post="$posts[1]"/>
-            @endforeach
-        </div>
     </main>
     {{-- @foreach ($posts as $post)
         <article>
