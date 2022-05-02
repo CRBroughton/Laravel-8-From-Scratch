5515857daf79c34d9b96c971eeff0aa8e67d5642
CRBroughton <crbroughton@posteo.uk>
2022-05-02 11:59:17 +0100

    refactored dropdown into components

---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,12 @@
+@props(['trigger'])
+<div x-data="{ show: false }" class="w-full" @click.away="show = false">
+    {{-- Trigger --}}
+    <div @click="show = !show">
+        {{ $trigger }}
+    </div>
+
+    {{-- Dropdown Links --}}
+    <div class="py-2 lg:absolute bg-gray-100 mt-2 rounded-xl w-full z-50" x-show="show">
+        {{ $slot }}
+    </div>
+</div>
