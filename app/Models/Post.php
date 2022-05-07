4c459ad17aa39a44e9c26284f6d8549f97fe7b3d
CRBroughton <crbroughton@posteo.uk>
2022-05-07 12:21:41 +0100

    moved search query to scoped filter

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,6 +11,14 @@ class Post extends Model
 
     protected $guarded = [];
 
+    public function scopeFilter($query, array $filters) {
+        if ($filters['search'] ?? false) {
+            $query->where('title', 'like', '%' . request('search') . '%')
+                  ->orWhere('body', 'like', '%' . request('search') . '%');
+    
+        }
+    }
+
     public function category() {
         return $this->belongsTo(Category::class);
     }
