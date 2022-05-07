f7752ec831fc9d44d2a59e0712d25568128c6e4f
CRBroughton <crbroughton@posteo.uk>
2022-05-07 12:10:30 +0100

    imported PostController

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Models\Post;
 use App\Models\User;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
