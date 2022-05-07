06e413056796db55feda88995290e596e1b94eba
CRBroughton <crbroughton@posteo.uk>
2022-05-07 12:05:22 +0100

    moved / route logic to PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index() {
+        $posts = Post::latest();
+
+        if (request('search')) {
+            $posts->where('title', 'like', '%' . request('search') . '%')
+                  ->orWhere('body', 'like', '%' . request('search') . '%');
+    
+        }
+        return view('posts', [
+            'posts' => $posts->get(),
+            'categories' => Category::all()
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,19 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $posts = Post::latest();
-
-    if (request('search')) {
-        $posts->where('title', 'like', '%' . request('search') . '%')
-              ->orWhere('body', 'like', '%' . request('search') . '%');
-
-    }
-    return view('posts', [
-        'posts' => $posts->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
+Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
